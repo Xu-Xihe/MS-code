8ff3ca195f942f3be4cfcd329b60d84f43389096
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 20:16:09 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
+#include "BusOut.h"
 #include "mbed.h"
+#include "mbed_thread.h"
 
-// main() runs in its own thread in the OS
+BusOut dig(D11,D3,D4,D5,D6,D9,D10);
+#define wait 1000
+
+const int opt[]={119,68,107,110,92,62,63,100,127,126,93,59,19,121,0};
 int main()
 {
+    int i=0;
     while (true) {
-
+        dig=opt[i];
+        thread_sleep_for(wait);
+        i++;
+        i%=15;
     }
 }
 

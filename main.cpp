10374cf9f0f13dca8e82bf5e1f4354c64c19074c
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 20:46:09 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
+#include "AnalogOut.h"
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+AnalogOut sawtooth(A3);
+
 int main()
 {
     while (true) {
-
+        for (int i=0 ; i <= 100 ; i++ ) {
+            sawtooth=(float)3/(float)3.3*(float)i/(float)100;
+            wait_us(85);
+        }
     }
 }
 

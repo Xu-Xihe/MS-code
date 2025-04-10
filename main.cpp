c71a26eeefa84b353baab4263af0b79088f1e381
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 13:17:24 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "PinNames.h"
+#include "PwmOut.h"
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+
+// Blinking rate in milliseconds
+#define Perid     500ms
+
+
 int main()
 {
-    while (true) {
-
+    PwmOut myout(D5);
+    myout.write(0.5f);
+    myout.period_ms(1);
+    while (1) {
     }
 }
-

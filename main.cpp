b9af68763306fa3b25a34a055ed675228447fb30
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 15:45:04 +0800

    debug

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
+#include "DigitalOut.h"
+#include "PinNames.h"
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
 int main()
 {
+    DigitalOut A(D5,1),B(D3,0),G(D10,0);
     while (true) {
-
+        A=1;
+        wait_us(500);
+        /*
+        G=0;
+        A=0;
+        B=0;
+        wait_us(500);
+        A=1;
+        wait_us(500);
+        B=1;
+        wait_us(500);
+        A=0;
+        wait_us(500);*/
     }
 }
 

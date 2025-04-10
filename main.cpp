62d04989df6f9eb33e28ee0e98bd99145dd84dc7
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 21:24:51 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
+#include "AnalogIn.h"
+#include "BusOut.h"
+#include "DigitalOut.h"
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+AnalogIn inpt(A6);
+BusOut light(D3,D4,D5,D6);
+
 int main()
 {
     while (true) {
-
+        if (inpt.read()*(float)3.3<(float)0.6) {
+            light=0;
+        }
+        else if (inpt.read()*(float)3.3<(float)1.2){
+            light=1;
+        }
+        else if (inpt.read()*(float)3.3<(float)1.8){
+            light=3;
+        }
+        else if (inpt.read()*(float)3.3<(float)2.4){
+            light=7;
+        }
+        else {
+            light=15;
+        }
     }
 }
 

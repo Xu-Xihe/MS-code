bf545d7cf299c6cdee38ee385889b7f481d0658b
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 12:38:07 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     1000ms
+
+
 int main()
 {
+    DigitalOut red(D5),green(D6);
     while (true) {
-
+        red=1;
+        green=0;
+        ThisThread::sleep_for(BLINKING_RATE);
+        red=0;
+        green=1;
+        ThisThread::sleep_for(BLINKING_RATE);
     }
 }
-

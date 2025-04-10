c8905adeac707db0a865157fb17f92703c9a41c0
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-04-10 20:29:34 +0800

    pass

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+#include "AnalogOut.h"
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+AnalogOut outpin(A3);
+
+const float opt[]={0.5,1,2,2.5};
+
 int main()
 {
+    int i=0;
     while (true) {
-
+        outpin=opt[i]/(float)3.3;
+        ThisThread::sleep_for(2s);
+        i++;
+        i%=4;
     }
 }
-

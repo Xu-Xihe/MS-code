d998f00e9ac011ac76197cbac4e6286065cc73e2
StarStreammm <starstreammm@StarStreammms-MacBook-Pro.local>
2025-05-04 15:51:51 +0800

    commented

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
 #include "mbed.h"
-
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     1000ms
 
-
 int main()
 {
-    DigitalOut red(D5),green(D6);
+    DigitalOut red(D5),green(D6); //select D5 to be red LED and D6 to be the green one
     while (true) {
-        red=1;
+        red=1; //set the red one on and the green one off
         green=0;
-        ThisThread::sleep_for(BLINKING_RATE);
-        red=0;
+        ThisThread::sleep_for(BLINKING_RATE); //wait for the next round
+        red=0; //visa versa
         green=1;
         ThisThread::sleep_for(BLINKING_RATE);
     }
